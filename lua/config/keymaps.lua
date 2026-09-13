b932fa775b1ba9f3b098c2c97927a58b385de48b
august <sms1543saud@gmail.com>
2026-09-13 09:19:59 +0300

    V1.1

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,20 @@ local map = vim.keymap.set
 
 -- Baisc
 map("n", "<leader>w", ":w!<cr>")
-map("n", "<leader>q", ":bdelete<cr>")
+map("n", "<leader>q", ":q!<cr>")
 map("n", "<esc>", ":noh<cr>")
+
+-- Splits
+map("n", "<leader>v", ":vsplit<cr>")
+map("n", "<leader>h", ":split<cr>")
+map("n", "<c-h>", "<c-w>h")
+map("n", "<c-j>", "<c-w>j")
+map("n", "<c-k>", "<c-w>k")
+map("n", "<c-l>", "<c-w>l")
+
+
+-- Terminal
+map("t", "<esc><esc>", "<c-\\><c-n>")
 
 -- Oil
 map("n", "<leader>e", require("oil").toggle_float)
