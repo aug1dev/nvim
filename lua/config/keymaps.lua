d8080a27ba6320ec961a2cd6af1f5f162d2d61ae
august <sms1543saud@gmail.com>
2026-09-22 22:09:49 +0300

    1.7V More keymaps for lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,11 +8,16 @@ map("n", "<esc>", ":noh<cr>")
 -- Splits
 map("n", "<leader>v", ":vsplit<cr>")
 map("n", "<leader>h", ":split<cr>")
+
 map("n", "<c-h>", "<c-w>h")
 map("n", "<c-j>", "<c-w>j")
 map("n", "<c-k>", "<c-w>k")
 map("n", "<c-l>", "<c-w>l")
 
+map("n", "<c-left>", ":vertical resize -2<cr>")
+map("n", "<c-down>", ":resize +2<cr>")
+map("n", "<c-up>", ":resize -2<cr>")
+map("n", "<c-right>", ":vertical resize +2<cr>")
 
 -- Terminal
 map("t", "<esc><esc>", "<c-\\><c-n>")
@@ -35,3 +40,13 @@ map("n", "<s-tab>", ":bprevious<cr>")
 -- Lsp
 map("n", "<leader>d", vim.diagnostic.open_float)
 map("n", "<leader>ca", vim.lsp.buf.code_action)
+
+map("n", "gd", vim.lsp.buf.definition)
+map("n", "gD", vim.lsp.buf.declaration)
+map("n", "gi", vim.lsp.buf.implementation)
+map("n", "gr", vim.lsp.buf.references)
+map("n", "gy", vim.lsp.buf.type_definition)
+
+map("n", "K", vim.lsp.buf.hover)
+
+map("n", "<leader>rn", vim.lsp.buf.rename)
